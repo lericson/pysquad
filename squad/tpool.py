49eeeb8c22dc46b3fe382021786a24452985eda7
Ludvig Ericson <ludvig@lericson.se>
2018-09-28 14:27:06 +0200

    [tpool] Improve job stopping by preempting jobs
    This uses the Python C API to set exceptions in other threads
    asynchronously. I am aware that this is likely to cost me my soul and my
    place in heaven, but if so, let the devil take me — this is too good.

--- squad/tpool.py
+++ squad/tpool.py
@@ -6,22 +6,34 @@ from itertools import chain
 from collections import namedtuple
 from multiprocessing import cpu_count
 
-from .utis import env_param, forward_exceptions
+from .utis import env_param, forward_exceptions, raise_in_threads
 
 NUM_THREADS = env_param('num_threads', default=cpu_count()-1, cast=int)
 
 log = logging.getLogger(__name__)
 R = namedtuple('R', 'ok num retval')
 
+
+class Stop(Exception):
+    pass
+
 class WorkerSet():
+    # If enabled, the workers will stop processing items if any job raises an
+    # exception. Note that the workers stop after the current job is done,
+    # unless preemption is enabled.
     stop_on_error = True
+
+    # Preemptive stopping raises an exception inside the current job to stop it
+    # more immediately, in a similar way to how KeyboardInterrupt will raise an
+    # exception in the currently executing frame.
+    preemptive_stop = True
 
     def __init__(self, func, *queues, num_threads=NUM_THREADS):
         self.func = func
         self._lock = threading.Lock()
         self._queue = enumerate(chain(*queues))
         self._workers = [threading.Thread(target=self._worker, daemon=True)
-                        for i in range(num_threads)]
+                         for i in range(num_threads)]
         self._results = []
 
     def start(self):
@@ -35,16 +47,22 @@ class WorkerSet():
 
     def stop(self):
         self._queue = iter(())
-        self.join()
+        if self.preemptive_stop:
+            raise_in_threads(Stop, *self._workers)
 
     def results(self):
         self._results.sort(key=lambda r: r.num)
         for r in self._results:
             if not r.ok:
-                raise r.retval
-            yield r.retval
+                try:
+                    raise r.retval
+                except Stop:
+                    pass
+            else:
+                yield r.retval
 
     def _worker(self):
+        name = str(self._workers.index(threading.current_thread()))
         while True:
             try:
                 with self._lock:
@@ -53,12 +71,15 @@ class WorkerSet():
                 break
             try:
                 retval = self.func(job)
+            except Stop as e:
+                self._results.append(R(False, num, e))
+                log.warning('[%s] job stopped', name)
             except Exception as e:
                 self._results.append(R(False, num, e))
-                log.error('job failed: %s\nfunc: %s\njob: %s',
-                          e, self.func, job)
+                log.error('[%s] job failed: %s\nfunc: %s\njob: %s',
+                          name, e, self.func, job)
                 if self.stop_on_error:
-                    self._queue = iter(())
+                    self.stop()
             else:
                 self._results.append(R(True, num, retval))
 
